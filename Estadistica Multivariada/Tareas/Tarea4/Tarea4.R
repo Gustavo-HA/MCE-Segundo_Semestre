2b55d4f86f412db26cbc133e0cba4e82304beb8d
Gustavo-HA <gustavoha40@gmail.com>
2025-04-07 18:15:39 -0600

    Avance T4-EM

--- Estadistica Multivariada/Tareas/Tarea4/Tarea4.R
+++ Estadistica Multivariada/Tareas/Tarea4/Tarea4.R
@@ -9,16 +9,5 @@ mu <- matrix(c(0,1,1), nrow=3)
 sigma <- matrix(c(3,-4,2,
                   -4,12,-2,
                   2,-2,3), nrow=3)
-R <- diag(diag(1/sigma)**0.5) %*% sigma %*% diag(diag(1/sigma)**0.5) 
 
-pca <- princomp(R)
-screeplot(pca)
-
-L <- matrix(pca$loadings[,1], nrow=3)
-comun <- diag((L%*%t(L)))
-
-varesp <- diag(1-comun)
-
-varesp
-
-L %*% t(L) + varesp
+pca <- prcomp(sigma)
